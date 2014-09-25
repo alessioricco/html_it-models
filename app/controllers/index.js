12e1f1ad0a96f0d5f8d6554f31b6d22452dfe9da
alessio ricco <alessio.ricco@gmail.com>
2014-09-26 00:08:47 +0200

    Models e Collezioni

---
+++ app/controllers/index.js
@@ -0,0 +1,61 @@
+var index = 0;
+var books = Alloy.createCollection("book");
+books.fetch();
+
+function save(e) {
+
+	// Crea un modello di tipo 'book'
+	var book = Alloy.createModel('book', {
+		title : $.title.value,
+		author : $.author.value,
+		isbn : $.isbn.value
+	});
+
+	// aggiungi un modello alla collezione
+	books.add(book);
+
+	// salva il modello
+	book.save();
+	index++;
+
+	// carica la collezione
+	//books.fetch();
+
+	$.title.value = "";
+	$.author.value = "";
+	$.isbn.value = "";
+
+	alert("inserimento completato");
+
+}
+
+function prev() {
+	index--;
+	if (index <= 0) {
+		index = 0;
+	}
+	showModel(index);
+}
+
+function next() {
+	index++;
+	if (index >= books.length) {
+		index = books.length - 1;
+	}
+	showModel(index);
+}
+
+function showModel(index) {
+	
+	//books.fetch();
+
+	var model = books.at(index);
+	
+	Ti.API.info(JSON.stringify(model));
+	
+	$.title.value = model.get("title") || "";
+	$.author.value = model.get("author") || "";
+	$.isbn.value = model.get("isbn") || "";
+}
+
+$.index.open();
---
+++ app/models/book.js
@@ -0,0 +1,27 @@
+exports.definition = {
+	config: {
+		columns: {
+		    "title": "string",
+		    "author": "string",
+		    "isbn": "string"
+		},
+		adapter: {
+			type: "sql",
+			collection_name: "book"
+		}
+	},
+	extendModel: function(Model) {
+		_.extend(Model.prototype, {
+			// extended functions and properties go here
+		});
+
+		return Model;
+	},
+	extendCollection: function(Collection) {
+		_.extend(Collection.prototype, {
+			// extended functions and properties go here
+		});
+
+		return Collection;
+	}
+};
